c1ebd7e5f31af27a7beca27edc9cb397f6c59855
pj-wise <pjwisedev@gmail.com>
2020-03-02 12:33:40 -0500

    search and Post comps rendered with props

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ Look at each file to see where you need to pass props or add code
 import React from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
-
+import SearchBar from "./components/SearchBar/SearchBarContainer";
+import Posts from "./components/PostsContainer/PostsPage";
 
 const App = () => {
-
   return (
-    <div className="App">
+    <div className='App'>
       {/* Add imported components here to render them */}
+      <SearchBar />
+      <Posts />
     </div>
   );
 };
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -3,16 +3,19 @@
 import React from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+// import data
+import dummyData from "../../dummy-data";
 
 const PostsPage = () => {
   // set up state for your data
   return (
-    <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+    <div className='posts-container-wrapper'>
+      {/* map through data here to return a Post and pass data as props to Post */
+      dummyData.map(data => (
+        <Post post={data} />
+      ))}
     </div>
   );
 };
 
 export default PostsPage;
-
